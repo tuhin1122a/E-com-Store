c146b7d6dc90bfade95f06116e1364fee37a1cf1
Tuhinur Rahman <tuhinrahmna48@gmail.com>
2025-07-09 13:06:31 +0600

    bug fixd

--- auth.js
+++ auth.js
@@ -1,82 +1,3 @@
-// import NextAuth from "next-auth";
-// import Credentials from "next-auth/providers/credentials";
-
-// export const { handlers, signIn, signOut, auth } = NextAuth({
-//   trustHost: true,
-//   providers: [
-//     Credentials({
-//       name: "Credentials",
-//       async authorize(credentials) {
-//         const res = await fetch(
-//           `${process.env.NEXT_PUBLIC_API_URL}/auth/login`,
-//           {
-//             method: "POST",
-//             headers: { "Content-Type": "application/json" },
-//             body: JSON.stringify({
-//               email: credentials.email,
-//               password: credentials.password,
-//             }),
-//           }
-//         );
-
-//         const data = await res.json();
-
-//         if (!res.ok || !data?.data?.user) return null;
-
-//         const user = data.data.user;
-
-//         return {
-//           id: user.id,
-//           name: user.firstName + " " + user.lastName,
-//           email: user.email,
-//           image: user.avatarUrl,
-//           role: user.role,
-//           accessToken: data.data.accessToken,
-//         };
-//       },
-//     }),
-//   ],
-
-//   pages: {
-//     signIn: "/login",
-//   },
-
-//   session: {
-//     strategy: "jwt",
-//   },
-
-//   callbacks: {
-//     async jwt({ token, user }) {
-//       // First login time: attach data from user
-//       if (user) {
-//         token.id = user.id;
-//         token.name = user.name;
-//         token.email = user.email;
-//         token.image = user.image;
-//         token.role = user.role;
-//         token.accessToken = user.accessToken;
-//       }
-
-//       return token;
-//     },
-
-//     async session({ session, token }) {
-//       if (token) {
-//         session.user = {
-//           id: token.id,
-//           name: token.name,
-//           email: token.email,
-//           image: token.image,
-//           role: token.role,
-//           accessToken: token.accessToken,
-//         };
-//       }
-//       return session;
-//     },
-//   },
-
-//   secret: process.env.NEXTAUTH_SECRET,
-// });
 import NextAuth from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
 import GoogleProvider from "next-auth/providers/google";
@@ -100,6 +21,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
         );
 
         const data = await res.json();
+        console.log("🟥 oauth-login status:", res.status);
 
         if (!res.ok || !data?.data?.user) return null;
 
